11ef07026b8ea79ebdf49d1779c15808715fc29f
Hans Zhou <hans.s.zhou@gmail.com>
2014-11-21 12:12:39 -0600

    Less dumb front page

--- www/index.php
+++ www/index.php
@@ -1,7 +1,8 @@
 <html>
 <head>
+    <title>My apologies for the ugly webpage</title>
 </head>
 <body>
-<?= "My Body is Ready" ?>
+    My name is Hans. I am a programmer. I am not a designer. This is where I keep all my dumb stuff on the internet.
 </body>
 </html>
